5554bbf83512b496435da89cd05a7969b6d378f0
Fernando Borretti <eudoxiahp@gmail.com>
2014-09-27 19:20:24 -0300

    Add GMail to a list of well-known services

--- src/services.lisp
+++ src/services.lisp
@@ -17,3 +17,16 @@
    (domains :reader domains :initarg :domains :type (list-of string))
    (smtp-server :reader smtp-server :initarg :smtp-server :type <smtp-server>)
    (imap-server :reader smtp-server :initarg :imap-server :type <imap-server>)))
+
+;;; Well-known services
+
+(defparameter +well-known-services+
+  (list
+   (make-instance '<service>
+                  :name "GMail"
+                  :domains (list "gmail.com" "googlemail.com")
+                  :smtp-server (make-instance '<smtp-server>
+                                              :host "smtp.gmail.com"
+                                              :ssl :tls)
+                  :imap-server (make-instance '<imap-server>
+                                              :host "imap.gmail.com"))))
