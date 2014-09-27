fdf5d730ed0dba8a277ae597874473bab05be632
Fernando Borretti <eudoxiahp@gmail.com>
2014-09-27 18:49:13 -0300

    Change <email-sender> to <sender-account>

--- src/send.lisp
+++ src/send.lisp
@@ -1,14 +1,12 @@
 (in-package :cl-user)
 (defpackage postmaster.send
-  (:use :cl :trivial-types :postmaster.email)
+  (:use :cl :postmaster.email)
   (:export :<smtp-server>
            :host
            :port
            :ssl
            :define-smtp-server))
 (in-package :postmaster.send)
-
-;;; SMTP configuration
 
 (defclass <smtp-server> ()
   ((host :reader host :initarg :host :type string)
@@ -32,14 +30,14 @@
                (t
                 (error "Unknown SSL preference ~A." (ssl server)))))))
 
-(defclass <email-sender> ()
+(defclass <sender-account> ()
   ((server :reader server :initarg :server :type <smtp-server>)
    (auth-method :reader auth-method :initarg :auth-method :type (or null keyword))
    (username :reader username :initarg :username :type string)
    (password :reader password :initarg :password :type string)))
 
-(defmethod send ((sender <email-sender>) (email <email>))
-  (let ((server (server sender)))
+(defmethod send ((account <sender-account>) (email <email>))
+  (let ((server (server account)))
     (cl-smtp:send-email (host server)
                         (from email)
                         (to email)
@@ -48,10 +46,10 @@
                         :ssl (ssl server)
                         :port (port server)
                         :html-message (html-body email)
-                        :authentication (if (auth-method sender)
-                                            (list (auth-method-sender)
-                                                  (username sender)
-                                                  (password sender))
-                                            (list (username sender)
-                                                  (password sender)))
+                        :authentication (if (auth-method account)
+                                            (list (auth-method account)
+                                                  (username account)
+                                                  (password account))
+                                            (list (username account)
+                                                  (password account)))
                         :attachments (convert-attachment-list (attachments email)))))
