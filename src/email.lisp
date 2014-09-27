14fa331beb9cf47eb94a1a7a108577d34e4a0b97
Fernando Borretti <eudoxiahp@gmail.com>
2014-09-27 18:19:13 -0300

    Add attachment class

--- src/email.lisp
+++ src/email.lisp
@@ -9,9 +9,16 @@
            :html-body))
 (in-package :postmaster.email)
 
+(defclass <attachment> ()
+  ((name :reader name :initarg :name :type string)
+   (path :reader path :initarg :path :type pathname)
+   (mime-type :reader mime-type :initarg :mime-type :type string)))
+
 (defclass <email> ()
   ((from :reader from :initarg :from :type string)
    (to :reader to :initarg :to :type (or string (list-of string)))
    (subject :reader subject :initarg :subject :type string)
    (body :reader body :initarg :body :type string)
-   (html-body :reader html-body :initarg :html-body :type string)))
+   (html-body :reader html-body :initarg :html-body :type string)
+   (attachments :reader attachments :initarg :attachments
+                :type (list-of <attachment>))))
