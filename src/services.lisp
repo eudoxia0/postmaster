a1e40a0ade6cd30b3eca23b5d3abd22678781b42
Fernando Borretti <eudoxiahp@gmail.com>
2014-09-28 20:19:20 -0300

    Make +well-known-services+ a list again

--- src/services.lisp
+++ src/services.lisp
@@ -1,6 +1,6 @@
 (in-package :cl-user)
 (defpackage postmaster.services
-  (:use :cl :trivial-types :anaphora)
+  (:use :cl :trivial-types)
   (:import-from :postmaster.servers
                 :<smtp-server>
                 :<imap-server>)
@@ -35,28 +35,24 @@
 ;;; Well-known services
 
 (defparameter +well-known-services+
-  (let ((table (make-hash-table)))
-    (setf (gethash :gmail table)
-          (make-instance '<service>
-                         :name "GMail"
-                         :domains (list "gmail.com" "googlemail.com")
-                         :smtp-server (make-instance '<smtp-server>
-                                                     :host "smtp.gmail.com"
-                                                     :ssl :tls)
-                         :imap-server (make-instance '<imap-server>
-                                                     :host "imap.gmail.com")))
-    table))
+  (list
+   (make-instance '<service>
+                  :name "GMail"
+                  :domains (list "gmail.com" "googlemail.com")
+                  :smtp-server (make-instance '<smtp-server>
+                                              :host "smtp.gmail.com"
+                                              :ssl :tls)
+                  :imap-server (make-instance '<imap-server>
+                                              :host "imap.gmail.com"))))
 
 (defun find-service-by-name (name)
-  "Find a service by name (A keyword)."
-  (multiple-value-bind (val foundp) (gethash name +well-known-services+)
-    ;; We do this so the output doesn't leak the fact that the service DB is a
-    ;; hash table
-    val))
+  "Find a service by name (A case-insensitive string)."
+  (loop for service in +well-known-services+ do
+    (if (equalp name (name service))
+        (return-from find-service-by-name service))))
 
 (defun find-service-by-domain (domain)
   "Find a service by its domain name. Case insensitive."
-  (loop for name being the hash-keys of +well-known-services+ do
-    (let ((service (find-service-by-name name)))
-      (if (member domain (domains service) :test #'equalp)
-          (return-from find-service-by-domain service)))))
+  (loop for service in +well-known-services+ do
+    (if (member domain (domains service) :test #'equalp)
+        (return-from find-service-by-domain service))))
